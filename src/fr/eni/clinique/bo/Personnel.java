e28cf68f866b071664a5a7d6669b96ea7245671c
lbaltimore2017 <lbaltimore2017@12-UC29-27.ad.campus-eni.fr>
2018-07-16 14:46:34 +0200

    attribut /getter et setter / constructeur client et personel

--- src/fr/eni/clinique/bo/Personnel.java
+++ src/fr/eni/clinique/bo/Personnel.java
@@ -1,5 +1,52 @@
 package fr.eni.clinique.bo;
 
 public class Personnel {
+	private int CodePers;
+	private String Nom;
+	private String MotPasse; 
+	private String Role;
+	private Boolean Archive;
+	
+	//CONSTRUCTEUR 
+		public Personnel(int codePers, String nom, String motPasse, String role) {
+			super();
+			CodePers = codePers;
+			Nom = nom;
+			MotPasse = motPasse;
+			Role = role;
+		}
 
+	// GETTER AND SETTER
+	public int getCodePers() {
+		return CodePers;
+	}
+	public void setCodePers(int codePers) {
+		CodePers = codePers;
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public String getMotPasse() {
+		return MotPasse;
+	}
+	public void setMotPasse(String motPasse) {
+		MotPasse = motPasse;
+	}
+	public String getRole() {
+		return Role;
+	}
+	public void setRole(String role) {
+		Role = role;
+	}
+	public Boolean getArchive() {
+		return Archive;
+	}
+	public void setArchive(Boolean archive) {
+		Archive = archive;
+	}
+	
+	
 }
