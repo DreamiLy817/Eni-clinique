a0a0d91556381f817fd69f3400eedbd2abb8b30c
lbaltimore2017 <lbaltimore2017@12-UC29-27.ad.campus-eni.fr>
2018-07-16 14:46:49 +0200

    fail commit

--- src/fr/eni/clinique/bo/Client.java
+++ src/fr/eni/clinique/bo/Client.java
@@ -1,5 +1,136 @@
 package fr.eni.clinique.bo;
 
 public class Client {
+	private int CodeClient;
+	private String Client;
+	private String PrenomClient;
+	private String Adresse1;
+	private String Adresse2;
+	private String CodePostal;
+	private String Ville;
+	private String NumTel;
+	private String Assurance;
+	private String Email;
+	private String Remarque;
+	private Boolean Archive;
+	
+	//CONSTRUCTEUR 
+	public Client(int codeClient, String client, String prenomClient, String adresse1, String codePostal, String ville,
+			String numTel, String assurance, String email) {
+		super();
+		CodeClient = codeClient;
+		Client = client;
+		PrenomClient = prenomClient;
+		Adresse1 = adresse1;
+		CodePostal = codePostal;
+		Ville = ville;
+		NumTel = numTel;
+		Assurance = assurance;
+		Email = email;
+	}
+
+	
+	// GETTE ET SETTER 
+	public int getCodeClient() {
+		return CodeClient;
+	}
+
+	public void setCodeClient(int codeClient) {
+		CodeClient = codeClient;
+	}
+
+	public String getClient() {
+		return Client;
+	}
+
+	public void setClient(String client) {
+		Client = client;
+	}
+
+	public String getPrenomClient() {
+		return PrenomClient;
+	}
+
+	public void setPrenomClient(String prenomClient) {
+		PrenomClient = prenomClient;
+	}
+
+	public String getAdresse1() {
+		return Adresse1;
+	}
+
+	public void setAdresse1(String adresse1) {
+		Adresse1 = adresse1;
+	}
+
+	public String getAdresse2() {
+		return Adresse2;
+	}
+
+	public void setAdresse2(String adresse2) {
+		Adresse2 = adresse2;
+	}
+
+	public String getCodePostal() {
+		return CodePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		CodePostal = codePostal;
+	}
+
+	public String getVille() {
+		return Ville;
+	}
+
+	public void setVille(String ville) {
+		Ville = ville;
+	}
+
+	public String getNumTel() {
+		return NumTel;
+	}
+
+	public void setNumTel(String numTel) {
+		NumTel = numTel;
+	}
+
+	public String getAssurance() {
+		return Assurance;
+	}
+
+	public void setAssurance(String assurance) {
+		Assurance = assurance;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getRemarque() {
+		return Remarque;
+	}
+
+	public void setRemarque(String remarque) {
+		Remarque = remarque;
+	}
+
+	public Boolean getArchive() {
+		return Archive;
+	}
+
+	public void setArchive(Boolean archive) {
+		Archive = archive;
+	} 
+	
+	
+	
+	
+	
+	
 
 }
