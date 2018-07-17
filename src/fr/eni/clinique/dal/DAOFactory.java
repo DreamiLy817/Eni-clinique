54f50571d8a7208f541c7766d3918dc7c19d8308
fbaconnais2018 <fbaconnais2018@12-UC29-17.ad.campus-eni.fr>
2018-07-17 14:18:14 +0200

    * DAL update

--- src/fr/eni/clinique/dal/DAOFactory.java
+++ src/fr/eni/clinique/dal/DAOFactory.java
@@ -1,14 +1,14 @@
 package fr.eni.clinique.dal;
 
 import fr.eni.clinique.bo.Login;
+import fr.eni.clinique.bo.Personnel;
 import fr.eni.clinique.dal.jdbc.LoginDAOJdbcImpl;
 import fr.eni.clinique.dal.jdbc.PersonnelDAOJdbcImpl;
 
 public class DAOFactory {
-	public static PersonnelDAO getPersonnelDAO() {
-		PersonnelDAO personnelDAO = new PersonnelDAOJdbcImpl();
-		return personnelDAO;
-	}
+//	public static DAO<Personnel> getPersonnelDAO() {
+//		return new PersonnelDAO();
+//	}
 
 //	public static PersonnelDAOFactory getLoginDAO() {
 //		DAO<Login> loginDAO = new LoginDAOJdbcImpl();
