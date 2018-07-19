6745f60ff3821c15226f0fd2e81465276a8a8143
lbaltimore2017 <lbaltimore2017@12-UC29-27.ad.campus-eni.fr>
2018-07-19 17:51:31 +0200

    ihm affichage client principal

---
+++ src/fr/eni/clinique/ihm/EcranClientPrincipal.java
@@ -0,0 +1,169 @@
+package fr.eni.clinique.ihm;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+
+public class EcranClientPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EcranClientPrincipal frame = new EcranClientPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EcranClientPrincipal() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 834, 484);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JButton btnRechercher = new JButton("rechercher");
+		GridBagConstraints gbc_btnRechercher = new GridBagConstraints();
+		gbc_btnRechercher.anchor = GridBagConstraints.WEST;
+		gbc_btnRechercher.insets = new Insets(0, 0, 5, 5);
+		gbc_btnRechercher.gridx = 1;
+		gbc_btnRechercher.gridy = 0;
+		contentPane.add(btnRechercher, gbc_btnRechercher);
+		
+		JButton btnAjouter = new JButton("ajouter");
+		GridBagConstraints gbc_btnAjouter = new GridBagConstraints();
+		gbc_btnAjouter.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAjouter.gridx = 3;
+		gbc_btnAjouter.gridy = 0;
+		contentPane.add(btnAjouter, gbc_btnAjouter);
+		
+		JButton btnSupprimer = new JButton("supprimer");
+		GridBagConstraints gbc_btnSupprimer = new GridBagConstraints();
+		gbc_btnSupprimer.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSupprimer.gridx = 4;
+		gbc_btnSupprimer.gridy = 0;
+		contentPane.add(btnSupprimer, gbc_btnSupprimer);
+		
+		JButton btnValider = new JButton("valider");
+		GridBagConstraints gbc_btnValider = new GridBagConstraints();
+		gbc_btnValider.insets = new Insets(0, 0, 5, 5);
+		gbc_btnValider.gridx = 6;
+		gbc_btnValider.gridy = 0;
+		contentPane.add(btnValider, gbc_btnValider);
+		
+		JButton btnAnnuler = new JButton("annuler");
+		GridBagConstraints gbc_btnAnnuler = new GridBagConstraints();
+		gbc_btnAnnuler.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAnnuler.gridx = 7;
+		gbc_btnAnnuler.gridy = 0;
+		contentPane.add(btnAnnuler, gbc_btnAnnuler);
+		
+		JLabel lblCode = new JLabel("Code");
+		GridBagConstraints gbc_lblCode = new GridBagConstraints();
+		gbc_lblCode.anchor = GridBagConstraints.WEST;
+		gbc_lblCode.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCode.gridx = 1;
+		gbc_lblCode.gridy = 2;
+		contentPane.add(lblCode, gbc_lblCode);
+		
+		JLabel lblNom = new JLabel("Nom");
+		GridBagConstraints gbc_lblNom = new GridBagConstraints();
+		gbc_lblNom.anchor = GridBagConstraints.WEST;
+		gbc_lblNom.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNom.gridx = 1;
+		gbc_lblNom.gridy = 3;
+		contentPane.add(lblNom, gbc_lblNom);
+		
+		JLabel lblPrenom = new JLabel("Prenom");
+		GridBagConstraints gbc_lblPrenom = new GridBagConstraints();
+		gbc_lblPrenom.anchor = GridBagConstraints.WEST;
+		gbc_lblPrenom.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPrenom.gridx = 1;
+		gbc_lblPrenom.gridy = 4;
+		contentPane.add(lblPrenom, gbc_lblPrenom);
+		
+		JLabel lblAdresse = new JLabel("Adresse");
+		GridBagConstraints gbc_lblAdresse = new GridBagConstraints();
+		gbc_lblAdresse.anchor = GridBagConstraints.WEST;
+		gbc_lblAdresse.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAdresse.gridx = 1;
+		gbc_lblAdresse.gridy = 5;
+		contentPane.add(lblAdresse, gbc_lblAdresse);
+		
+		JLabel lblCodePostal = new JLabel("Code postal");
+		GridBagConstraints gbc_lblCodePostal = new GridBagConstraints();
+		gbc_lblCodePostal.anchor = GridBagConstraints.WEST;
+		gbc_lblCodePostal.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCodePostal.gridx = 1;
+		gbc_lblCodePostal.gridy = 7;
+		contentPane.add(lblCodePostal, gbc_lblCodePostal);
+		
+		JLabel lblVille = new JLabel("Ville");
+		GridBagConstraints gbc_lblVille = new GridBagConstraints();
+		gbc_lblVille.anchor = GridBagConstraints.WEST;
+		gbc_lblVille.insets = new Insets(0, 0, 5, 5);
+		gbc_lblVille.gridx = 1;
+		gbc_lblVille.gridy = 8;
+		contentPane.add(lblVille, gbc_lblVille);
+		
+		JLabel lblNum = new JLabel("Num");
+		GridBagConstraints gbc_lblNum = new GridBagConstraints();
+		gbc_lblNum.anchor = GridBagConstraints.WEST;
+		gbc_lblNum.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNum.gridx = 1;
+		gbc_lblNum.gridy = 9;
+		contentPane.add(lblNum, gbc_lblNum);
+		
+		JLabel lblAssurance = new JLabel("Assurance");
+		GridBagConstraints gbc_lblAssurance = new GridBagConstraints();
+		gbc_lblAssurance.anchor = GridBagConstraints.WEST;
+		gbc_lblAssurance.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAssurance.gridx = 1;
+		gbc_lblAssurance.gridy = 10;
+		contentPane.add(lblAssurance, gbc_lblAssurance);
+		
+		JLabel lblEmail = new JLabel("Email");
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.anchor = GridBagConstraints.WEST;
+		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEmail.gridx = 1;
+		gbc_lblEmail.gridy = 11;
+		contentPane.add(lblEmail, gbc_lblEmail);
+		
+		JLabel lblRemarque = new JLabel("Remarque");
+		GridBagConstraints gbc_lblRemarque = new GridBagConstraints();
+		gbc_lblRemarque.anchor = GridBagConstraints.WEST;
+		gbc_lblRemarque.insets = new Insets(0, 0, 0, 5);
+		gbc_lblRemarque.gridx = 1;
+		gbc_lblRemarque.gridy = 12;
+		contentPane.add(lblRemarque, gbc_lblRemarque);
+	}
+
+}
